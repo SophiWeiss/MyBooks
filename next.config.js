a14d879f6a852c2b0301424cd8a42ff2d2ed562e
Papsanly <papsanlysenko@gmail.com>
2023-06-05 13:00:35 +0300

    moved license image to public

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.creativecommons.org',
-        port: '',
-        pathname: '/l/by-nc-nd/4.0/88x31.png'
-      }
-    ]
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
