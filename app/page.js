08a68598a3196120dce570573274d609e95885bc
Papsanly <papsanlysenko@gmail.com>
2023-06-05 13:08:32 +0300

    basic html layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,71 @@
 import Image from 'next/image'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
+function Title() {
+  return <h1>My Books</h1>
+}
 
-export default function Home() {
+function Arrow({ right, left }) {
   return (
-    <main>
-      <h1>My Books</h1>
+    <>
+      {right && <BsChevronRight />}
+      {left && <BsChevronLeft />}
+    </>
+  )
+}
+
+function BookCard({ title, description, status }) {
+  return (
+    <a>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <div data-status={status}>{status}</div>
+    </a>
+  )
+}
+
+function Dots() {
+  return (
+    <div>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}
+
+function Books() {
+  return (
+    <section>
+      <div>
+        <Arrow right />
+        <BookCard
+          title={'Book 1'}
+          description={
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do' +
+            ' eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut ' +
+            'enim ad minim veniam, quis nostrud exercitation ullamco'
+          }
+          status={'Done'}
+        />
+        <Arrow left />
+      </div>
+      <Dots />
+    </section>
+  )
+}
+
+function ModeSwitch() {
+  return (
+    <div>
+      <div />
+    </div>
+  )
+}
+
+function License() {
+  return (
+    <>
       <a
         rel="license"
         href="https://creativecommons.org/licenses/by-nc-nd/4.0/"
@@ -12,7 +74,7 @@ export default function Home() {
           priority
           alt="Creative Commons License"
           style="border-width:0"
-          src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
+          src="/by-nc-nd.svg"
           width={88}
           height={31}
         />
@@ -25,6 +87,27 @@ export default function Home() {
         Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
         International License.
       </a>
+    </>
+  )
+}
+
+function Ukraine() {
+  return (
+    <a>
+      <div />
+      <div />
+    </a>
+  )
+}
+
+export default function Home() {
+  return (
+    <main>
+      <Title />
+      <Books />
+      <ModeSwitch />
+      <License />
+      <Ukraine />
     </main>
   )
 }
