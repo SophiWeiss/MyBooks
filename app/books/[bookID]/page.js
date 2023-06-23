1ac3013ec50502866e3c833546786f401c7c8953
Papsanly <papsanlysenko@gmail.com>
2023-06-23 17:11:54 +0300

    added static params for books page

--- app/books/[bookID]/page.js
+++ app/books/[bookID]/page.js
@@ -1,3 +1,13 @@
+import { getSortedBooksData } from '@lib/books'
+
+export async function generateStaticParams() {
+  const books = await getSortedBooksData()
+
+  return books.map(book => ({
+    bookID: book.id
+  }))
+}
+
 export default function Book({ params }) {
   let { bookID } = params
   return (
---
+++ app/books/page.js
@@ -0,0 +1,7 @@
+'use client'
+
+import style from './page.module.css'
+
+export default function AllBooks() {
+  return <h1>All Books</h1>
+}
