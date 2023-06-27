5b486623c366c13290a8185b8dc2ef7fdce75426
Sophi Weiss <Owlety34@gmail.com>
2023-06-27 12:39:53 +0200

    rewritten readMd function

--- app/ukraine/page.js
+++ app/ukraine/page.js
@@ -1,15 +1,12 @@
 import { readMd } from '@lib/markdown'
 import Markdown from '@/components/Markdown'
-import path from 'path'
 
 export const metadata = {
   title: 'Ukraine💙💛'
 }
 
-const mdPath = path.join(process.cwd(), path.join('content', 'ukraine.md'))
-
 export default async function Ukraine() {
-  const { content } = await readMd(mdPath)
+  const { content } = await readMd('ukraine.md')
   return (
     <article>
       <Markdown>{content}</Markdown>
