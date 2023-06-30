344234cceec2b78bdc326066fd068dc83cb683ca
Papsan <papsanlysenko@gmail.com>
2023-07-01 00:14:56 +0300

    refactored book list to separate component

--- src/app/(navigation)/books/page.js
+++ src/app/(navigation)/books/page.js
@@ -1,22 +1,18 @@
 import style from './page.module.css'
 import { getSortedBooksData } from '@/lib/books'
-import BookCard from '@/components/BookCard'
+import BookList from '@/components/BookList'
 
 export const metadata = {
   title: 'All Books'
 }
 
 export default async function AllBooks() {
-  const books = (await getSortedBooksData()).map(book => (
-    <li key={book.id}>
-      <BookCard book={book} />
-    </li>
-  ))
+  const books = await getSortedBooksData()
 
   return (
     <section className={style.allBooks}>
       <h1 className={style.header}>All Books</h1>
-      <ul className={style.booksList}>{books}</ul>
+      <BookList books={books} />
     </section>
   )
 }
