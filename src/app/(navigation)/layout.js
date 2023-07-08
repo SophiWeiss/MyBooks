87bb60c5a6b56d7577790e7daeb48141855630da
Papsan <papsanlysenko@gmail.com>
2023-07-09 01:09:20 +0300

    changed nav bar layout

--- src/app/(navigation)/layout.js
+++ src/app/(navigation)/layout.js
@@ -1,20 +1,13 @@
 import style from './layout.module.css'
-import { HiHome } from 'react-icons/hi2'
-import Link from 'next/link'
 import ThemeSwitch from '@/components/ThemeSwitch'
 
 export default function Navigation({ children }) {
   return (
-    <main className={style.main}>
-      <nav className={style.navLeft}>
-        <Link href={'/'} className={style.homeLink}>
-          <HiHome />
-        </Link>
-      </nav>
-      {children}
-      <nav className={style.navRight}>
+    <>
+      <nav className={style.nav}>
         <ThemeSwitch />
       </nav>
-    </main>
+      <main className={style.main}>{children}</main>
+    </>
   )
 }
