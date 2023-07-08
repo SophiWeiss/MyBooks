2954115c3ad4509fe5df68e3249853d898a95b60
Papsan <papsanlysenko@gmail.com>
2023-07-08 21:55:56 +0300

    made Ukraine flag resizable

--- src/components/Ukraine.js
+++ src/components/Ukraine.js
@@ -1,9 +1,13 @@
 import style from './css/Ukraine.module.css'
 import Link from 'next/link'
 
-export default function Ukraine() {
+export default function Ukraine({ size = 60 }) {
   return (
-    <Link href={'/ukraine'} className={style.Ukraine}>
+    <Link
+      href={'/ukraine'}
+      className={style.Ukraine}
+      style={{ width: `${size}px`, height: `${(size * 2) / 3}px` }}
+    >
       <div className={style.UkraineBlue} />
       <div className={style.UkraineYellow} />
     </Link>
