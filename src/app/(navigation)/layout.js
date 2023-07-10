b972ba0babcb24b4fca1961a1690a05b38fd2d23
Papsan <papsanlysenko@gmail.com>
2023-07-10 17:02:54 +0300

    added menu

--- src/app/(navigation)/layout.js
+++ src/app/(navigation)/layout.js
@@ -1,10 +1,34 @@
 import style from './layout.module.css'
+import Link from 'next/link'
+import { HiMenuAlt2 } from 'react-icons/hi'
 import ThemeSwitch from '@/components/ThemeSwitch'
+import Ukraine from '@/components/Ukraine'
+import DropDown from '@/components/DropDown'
 
 export default function Navigation({ children }) {
+  const links = [
+    { href: '/', component: 'Home' },
+    { href: '/books', component: 'Books' },
+    { href: '/about-me', component: 'About Me' },
+    { href: '/ukraine', component: <Ukraine size={20} link={false} /> }
+  ]
+
+  const linkElements = links.map(({ href, component }) => (
+    <Link href={href} key={href} className={style.linkElement}>
+      {component}
+    </Link>
+  ))
+
   return (
     <>
       <nav className={style.nav}>
+        <DropDown
+          direction={'row'}
+          align={'start'}
+          icon={<HiMenuAlt2 size={20} />}
+        >
+          {linkElements}
+        </DropDown>
         <ThemeSwitch />
       </nav>
       <main>{children}</main>
--- src/components/Ukraine.js
+++ src/components/Ukraine.js
@@ -1,15 +1,23 @@
 import style from './css/Ukraine.module.css'
 import Link from 'next/link'
 
-export default function Ukraine({ size }) {
-  return (
+export default function Ukraine({ size, link = true }) {
+  return link ? (
     <Link
       href={'/ukraine'}
-      className={style.Ukraine}
+      className={style.UkraineLink}
       style={{ width: `${size}px`, height: `${(size * 2) / 3}px` }}
     >
       <div className={style.UkraineBlue} />
       <div className={style.UkraineYellow} />
     </Link>
+  ) : (
+    <div
+      className={style.Ukraine}
+      style={{ width: `${size}px`, height: `${(size * 2) / 3}px` }}
+    >
+      <div className={style.UkraineBlue} />
+      <div className={style.UkraineYellow} />
+    </div>
   )
 }
