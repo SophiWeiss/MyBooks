e257370b67b92ff3281b07c695bd337cdbc39451
Papsanly <papsanlysenko@gmail.com>
2023-07-12 01:24:30 +0300

    added support for links in md files

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,6 +1,16 @@
 import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
 
 export default function Markdown(props) {
   const { children, ...other } = props
-  return <ReactMarkdown {...other}>{children}</ReactMarkdown>
+  return (
+    <ReactMarkdown
+      {...other}
+      components={{
+        a: ({ href, children }) => <Link href={href}>{children[0]}</Link>
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  )
 }
