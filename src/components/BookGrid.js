d76f56695513e29392ece5fe65ba911b484cf745
Papsanly <papsanlysenko@gmail.com>
2023-07-14 02:21:11 +0300

    made all books page more web crawlers friendly

--- src/components/BookGrid.js
+++ src/components/BookGrid.js
@@ -5,18 +5,21 @@ import { useEffect, useState } from 'react'
 import BookCard from './BookCard'
 
 export default function BookGrid({ books }) {
-  const bookElements = books.map(book => (
-    <BookCard style={{ maxWidth: 'unset' }} key={book.id} book={book} />
-  ))
-
   const [mounted, setMounted] = useState(false)
+
   useEffect(() => setMounted(true), [])
 
+  const bookElements = books.map(book => (
+    <BookCard
+      style={mounted ? { maxWidth: 'unset' } : { alignSelf: 'center' }}
+      key={book.id}
+      book={book}
+    />
+  ))
+
   return (
-    mounted && (
-      <ResponsiveMasonry columnsCountBreakPoints={{ 400: 1, 800: 2, 1200: 3 }}>
-        <Masonry gutter={'20px'}>{bookElements}</Masonry>
-      </ResponsiveMasonry>
-    )
+    <ResponsiveMasonry columnsCountBreakPoints={{ 400: 1, 800: 2, 1200: 3 }}>
+      <Masonry gutter={'20px'}>{bookElements}</Masonry>
+    </ResponsiveMasonry>
   )
 }
