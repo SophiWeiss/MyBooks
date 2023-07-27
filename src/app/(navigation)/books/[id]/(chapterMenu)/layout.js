8006232820f68f8df29bdd316e51e1e2252a2359
Papsan <papsanlysenko@gmail.com>
2023-07-27 22:52:57 +0300

    removed scroll bar, removed rerender of chapter menu

--- src/app/(navigation)/books/[id]/(chapterMenu)/layout.js
+++ src/app/(navigation)/books/[id]/(chapterMenu)/layout.js
@@ -2,24 +2,7 @@ import SideMenu from '@/components/SideMenu'
 import { BiSolidBookBookmark } from 'react-icons/bi'
 import Link from 'next/link'
 import commonStyle from '@/css/common.module.css'
-import { getBookContent, getBookData, getChapterContent } from '@/lib/books'
-
-export const dynamicParams = false
-
-export async function generateMetadata({ params }) {
-  const { id, chapter } = params
-  const { title: bookTitle } = await getBookData(id)
-  const { title: chapterTitle } = await getChapterContent(id, chapter)
-  return { title: `${bookTitle} | ${chapterTitle}` }
-}
-
-export async function generateStaticParams({ params }) {
-  const { id } = params
-  const chapters = await getBookContent(id)
-  return chapters.map(chapter => ({
-    chapter: chapter.index.toString()
-  }))
-}
+import { getBookContent } from '@/lib/books'
 
 export default async function ChapterLayout({ children, params }) {
   const { id } = params
